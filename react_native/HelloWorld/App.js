2191a41df426a1c68b8a241d389397abe7eb00e5
Ashraf Azemi <ashraf.chzm@gmail.com>
2023-08-01 15:22:13 +0800

    Button, Modal, Pressable, StatusBar, ActivityIndicator, Alert

--- react_native/HelloWorld/App.js
+++ react_native/HelloWorld/App.js
@@ -1,24 +1,87 @@
-import { View, Text, Image, ImageBackground, ScrollView } from "react-native";
+import React, { useState } from "react";
+import Greet from "./components/Greet";
+import {
+  View,
+  Text,
+  Image,
+  ImageBackground,
+  ScrollView,
+  Button,
+  Pressable,
+  Modal,
+  StatusBar,
+  ActivityIndicator,
+  Alert,
+} from "react-native";
 const logImg = require("./assets/adaptive-icon.png");
 
 const App = () => {
+  const [modal, setModal] = useState(false);
+  const [statusBar, setStatusBar] = useState(false);
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
+      <Greet name="Johny Sin" />
+      <Greet name="Adam Sandler" />
+      <Button
+        title="Alert Button"
+        onPress={() =>
+          Alert.alert("Invalid Data", "DOB is incorrect", [
+            { text: "cancel", onPress: () => console.log("cancel") },
+            { text: "ok", onPress: () => console.log("ok") },
+          ])
+        }
+      />
+      <ActivityIndicator color={"black"} size={"large"} animating={true} />
+      <StatusBar
+        backgroundColor={"green"}
+        barStyle="light-content"
+        hidden={statusBar}
+      />
+      <Button
+        title="toggle statusbar"
+        onPress={() => {
+          setStatusBar(!statusBar);
+        }}
+      />
       {/* <View
         style={{ width: 200, height: 200, backgroundColor: "lightblue" }}
-      ></View>
-      <View
+        ></View>
+        <View
         style={{ width: 200, height: 200, backgroundColor: "lightgreen" }}
-      ></View>
-      <View style={{ width: 200, height: 200, backgroundColor: "red" }}></View>
-      <Text>
+        ></View>
+        <View style={{ width: 200, height: 200, backgroundColor: "red" }}></View>
+        <Text>
         <Text style={{ color: "white" }}>Hello</Text> world
       </Text> */}
+      <Button
+        title="press"
+        onPress={() => alert("button pressed")}
+        color={"midnightblue"}
+        disabled
+      />
+      <Modal
+        visible={modal}
+        onRequestClose={() => setModal(false)}
+        animationType="slide"
+        presentationStyle="pageSheet"
+      >
+        <View style={{ flex: 1, backgroundColor: "lightblue", padding: 60 }}>
+          <Text>Modal Content</Text>
+          <Button
+            title="Close"
+            color="midnightblue"
+            onPress={() => setModal(false)}
+          />
+        </View>
+      </Modal>
       <ScrollView>
-        <Image
-          source={{ uri: "https://picsum.photos/400" }}
-          style={{ width: 300, height: 300 }}
-        />
+        <Pressable onPress={() => setModal(true)}>
+          <Image
+            source={{ uri: "https://picsum.photos/400" }}
+            style={{ width: 300, height: 300 }}
+          />
+        </Pressable>
+
         <Text>
           Contrary to popular belief, Lorem Ipsum is not simply random text. It
           has roots in a piece of classical Latin literature from 45 BC, making
